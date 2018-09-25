c45a30bb91cad264236198d7439ff0ba17f2894c
Martijn Laarman <Mpdreamz@gmail.com>
2018-09-25 18:21:03 +0200

    Add TestingClass.cs

---
+++ src/Examples/Elastic.Xunit.ExampleComplex/TestingClass.cs
@@ -0,0 +1,17 @@
+﻿using Elastic.Xunit.XunitPlumbing;
+using FluentAssertions;
+
+namespace Elastic.Xunit.ExampleComplex
+{
+	public class TestingClass : ClusterTestClassBase<TestCluster>
+	{
+		public TestingClass(TestCluster cluster) : base(cluster) { }
+
+		[I] public void SomeTest()
+		{
+			var info = this.Client.RootNodeInfo();
+
+			info.IsValid.Should().BeTrue();
+		}
+	}
+}
